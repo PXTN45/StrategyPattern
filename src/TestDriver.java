a28b83cf6395a29ed193c9b70d75826ced00a5ad
PXTN45 <120036577+PXTN45@users.noreply.github.com>
2023-09-25 21:57:27 +0700

    ...

---
+++ src/TestDriver.java
@@ -0,0 +1,12 @@
+
+public class TestDriver {
+	public static void main(String[] args) {
+		ShoppingCart cart1 = new ShoppingCart();
+		cart1.checkout(1500);
+		cart1.setPaymentStrategy(new CreditCardPayment());
+		cart1.checkout(10000);
+		cart1.setPaymentStrategy(new ScanPayment());
+		cart1.checkout(100000);
+	}
+
+}
